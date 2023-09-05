8f75447599740d6556b5325ca433976ada387d34
ismaelcc <143725692+ismaelcc@users.noreply.github.com>
2023-09-05 08:44:21 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listatareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    String tarea1 = " serie de ejercicios";
+    String tarea2 = " sumas de octales";
+    String tarea3 = " exposicion etica";
+    String tarea4 = " investigacion ";
+    String tarea5 = "algoritmos";
+    
+    
+   
+     System.out.println("la tarea 1 es: " + tarea1 );
+     System.out.println("la tarea 2 es: " + tarea2 );
+     System.out.println("la tarea 3 es: " + tarea3 );
+     System.out.println("la tarea 4 es: " + tarea4 );
+     System.out.println("la tarea 5 es:" + tarea5 );
+   
+    
+        
+    }
+    
+}
