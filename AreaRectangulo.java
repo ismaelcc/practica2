f592f84699c4d69904d91cf00e38bd3df711c9c4
ismaelcc <143725692+ismaelcc@users.noreply.github.com>
2023-09-07 08:47:24 -0600

    Add files via upload

---
+++ AreaRectangulo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaRectangulo {
+    public static void main(String[] args) {
+        
+        System.out.println("el nombre completo es ismael colin contreras ");
+        
+        int base = 30 ;
+        int altura = 20; 
+        int areaRectangulo = base * altura;
+        
+        System.out.println("el area del rectangulo es:" + areaRectangulo);
+    }
+    
+}
